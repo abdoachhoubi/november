3e6f3414d2a3274b2a556ebeaab2483e53061ee6
abdoachhoubi <aachhoub@gmail.com>
2024-03-17 02:32:40 +0000

    Just having fun LOL

---
+++ src/App.tsx
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from 'react';
+import video from './assets/videos/video.mp4';
+
+
+const Countdown: React.FC = () => {
+	const calculateTimeLeft = () => {
+		const now = new Date();
+		const targetDate = new Date('2024-11-02T00:00:00');
+		const difference = targetDate.getTime() - now.getTime();
+
+		let timeLeft = {
+			months: 0,
+			days: 0,
+			hours: 0,
+		};
+
+		if (difference > 0) {
+			timeLeft = {
+				months: Math.floor(difference / (1000 * 60 * 60 * 24 * 30)),
+				days: Math.floor((difference / (1000 * 60 * 60 * 24)) % 30),
+				hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+			};
+		}
+
+		return timeLeft;
+	};
+
+	const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			setTimeLeft(calculateTimeLeft());
+		}, 1000);
+
+		return () => clearTimeout(timer);
+	});
+
+	return (
+		<div className='countdown'>
+			<h1>Countdown to November 2, 2024</h1>
+			<div className='counter'>
+				<p>{timeLeft.months} <span>months</span></p><span>:</span>
+				<p>{timeLeft.days} <span>days</span></p><span>:</span>
+				<p>{timeLeft.hours} <span>hours</span></p>
+			</div>
+		</div>
+	);
+};
+
+
+function App() {
+	return (
+		<>
+			<video src={video} loop autoPlay muted className='video'></video>
+			<main className="home">
+				<h1 className='heading'>Meet Astro</h1>
+				<h2 className='subheading'>Born in November, owns <a className='link' href="https://november.ma">November</a></h2>
+				<Countdown />
+			</main>
+		</>
+	);
+}
+
+export default App;
